d524cf4500b7338d6c6fdb71e15bce676fbf7bd1
abey546 <abeyhassan546@gmail.com>
2023-06-15 11:56:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - program that prints a line to the standard error
  * Return 1 (Success)
- */ 
+ */
 int main(void)
 {
 Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
