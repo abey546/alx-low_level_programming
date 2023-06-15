a2c5e098cf1e64922ebcf5f057ba11c1f95e33c1
abey546 <abeyhassan546@gmail.com>
2023-06-15 11:49:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry Point
- * Return: Always 1 (Succes)
- */
+ * main - program that prints a line to the standard error
+ * Return 1 (Success)
+ */ 
 int main(void)
 {
 Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
