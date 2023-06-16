8e90def31e8b2c7467afe91b8aaf6066bec565f1
abey546 <abeyhassan546@gmail.com>
2023-06-16 12:23:01 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	int n = 97;
 
-	for (i = 0; i < 26; i++)
-{
-	putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
