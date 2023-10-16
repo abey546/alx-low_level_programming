c71aa098850c3c69f3cb733204c071182bcb77d2
abey546 <abeyhassan546@gmail.com>
2023-10-16 04:40:48 -0700

    python3 100-tests.py

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - adds two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return: sub of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return: multplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return:division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod -check modula of two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return: % of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
